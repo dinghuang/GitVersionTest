f98c38bf4b5c3c724245f1680c62e2392cee3d5a
dinghuang <huang.ding@hand-china.com>
2017-12-19 14:44:48 +0800

    first commit

---
+++ AssemblyInfo.cs
@@ -0,0 +1,4 @@
+
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0+Branch.master.Sha.d11f026874a1ce4bd3a5d2cb1f522243382239fa")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
